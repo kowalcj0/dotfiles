71609c9109117fcc1dd9338a25d2c79faee27357
Janusz Kowalczyk <kowalczykjanusz@gmail.com>
2013-11-20 10:28:06 +0000

    added byzanz scripts that allow to create GIF animated screencasts

---
+++ usr/bin/README.md
@@ -0,0 +1,18 @@
+
+
+
+
+Byzanz
+A nice to to create animated GIF screencasts
+More on this topic here: http://askubuntu.com/questions/107726/how-to-create-animated-gif-images-of-a-screencast
+
+To install byzans on Ubuntu etc.
+
+    sudo add-apt-repository ppa:fossfreedom/byzanz
+    sudo apt-get update && sudo apt-get install byzanz
+
+Then copy all byzans-record-*.sh to ie. /usr/bin
+byzanz-record-region*.sh scripts require xrectsel tool to be installed
+https://github.com/lolilolicon/FFcast2/blob/master/xrectsel.c
+
+
---
+++ usr/bin/byzanz-record-region-gui.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# AUTHOR:   (c) Rob W 2012, modified by MHC (http://askubuntu.com/users/81372/mhc)
+# NAME:     GIFRecord 0.1
+# DESCRIPTION:  A script to record GIF screencasts.
+# LICENSE:  GNU GPL v3 (http://www.gnu.org/licenses/gpl.html)
+# DEPENDENCIES:   byzanz,gdialog,notify-send (install via sudo add-apt-repository ppa:fossfreedom/byzanz; sudo apt-get update && sudo apt-get install byzanz gdialog notify-osd)
+
+# Time and date
+TIME=$(date +"%Y-%m-%d_%H%M%S")
+
+# Delay before starting
+DELAY=10
+
+# Standard screencast folder
+FOLDER="$HOME/Pictures"
+
+# Default recording duration
+DEFDUR=10
+
+# Sound notification to let one know when recording is about to start (and ends)
+beep() {
+    paplay /usr/share/sounds/freedesktop/stereo/message-new-instant.oga &
+}
+
+# Custom recording duration as set by user
+USERDUR=$(gdialog --title "Duration?" --inputbox "Please enter the screencast duration in seconds" 200 100 2>&1)
+
+# Duration and output file
+if [ $USERDUR -gt 0 ]; then
+    D=$USERDUR
+else
+    D=$DEFDUR
+fi
+
+# xrectsel from https://github.com/lolilolicon/FFcast2/blob/master/xrectsel.c
+ARGUMENTS=$(xrectsel "--x=%x --y=%y --width=%w --height=%h") || exit -1
+
+# Notify the user of recording time and delay
+notify-send "GIFRecorder" "Recording duration set to $D seconds. Recording will start in $DELAY seconds."
+
+echo Delaying $DELAY seconds. After that, byzanz will start
+for (( i=$DELAY; i>0; --i )) ; do
+    echo $i
+    sleep 1
+done
+beep
+
+
+#Actual recording
+beep
+byzanz-record --verbose --delay=0 --duration=$D ${ARGUMENTS} "$FOLDER/GIFrecord_$TIME.gif"
+beep
+
+# Notify the user of end of recording.
+notify-send "GIFRecorder" "Screencast saved to $FOLDER/GIFrecord_$TIME.gif"
